a0db6d1136218a61054d67707983fcf676d4c9d0
ayushi pal <ayushipal06@gmail.com>
2024-05-22 18:14:55 +0530

    UPDATED INDEX.JS

--- index.js
+++ index.js
@@ -18,11 +18,28 @@ const PORT =  5050;
 const app = express();
 const cors = require('cors');
 // Allow all origins
-app.use(cors());
-// Allow specific origin(s)
-app.use(cors({
-  origin: 'https://we-hear.vercel.app/'
-}));
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://we-hear.vercel.app/"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
+
+
 //middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
