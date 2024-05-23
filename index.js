a31bbef877dd213546b7e343fce8da36811b7544
ayushi pal <ayushipal06@gmail.com>
2024-05-23 09:01:52 +0530

    fixing bugs

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const bodyParser = require('body-parser')
 const db=require('./config/connection.js');
 const authRouter = require('./routes/auth.js')
 const cors = require('cors')
-const productRouter = require('./routes/ProductRoute.js')
-const cartRouter = require('./routes/CartRoutes.js')
-
 
 
 db();
@@ -17,35 +14,19 @@ const api = process.env.API_URL;
 const PORT = process.env.PORT || 4000;
 const app = express();
 
-const corsOptions = {
-  origin: 'https://we-hear.vercel.app',
-  optionsSuccessStatus: 200 // For legacy browser support
-};
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-// Enable CORS for all routes
-app.use(cors(corsOptions));
-
-// Enable pre-flight requests for all routes
-app.options('*', cors(corsOptions));
-
-// Allow all origins
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'https://we-hear.vercel.app'); // Allow the specific origin
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // Add any custom headers here
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow the methods you need
-//   next();
-// });
-
-const path = require("path");
-
-app.use(express.static(path.join(__dirname, "build")));
-
+app.use(express.json());
+app.use(express.static(__dirname));
 //middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(`${api}/user`,authRouter)
-app.use(`${api}/product`,productRouter)
-app.use(`${api}/cart`,cartRouter)
+
 
 
 app.use(notFound);
